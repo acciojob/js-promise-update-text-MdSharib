baae8bea434a2fe51e3eecc4354725970d067aeb
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-06-30 11:27:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const div = document.getElementById("output");
+
+let prom = new Promise((res, rej) => {
+	setTimeout(() => {
+		res("hello, world!");
+	}, 1000)
+})
+
+prom.then((data) => {
+	div.innerText = data;
+})
